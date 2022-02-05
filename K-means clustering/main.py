4416fc08234b18ae625a3fbf2f7a78582df2f208
Киселёв Андрей <vkusnaya.travka@gmail.com>
2022-02-05 20:36:41 +0300

    code reduction

--- K-means clustering/main.py
+++ K-means clustering/main.py
@@ -11,8 +11,6 @@ if (len(sys.argv) < 3) or not sys.argv[1].isdigit() or not sys.argv[2].isdigit()
 k = int(sys.argv[1])
 n = int(sys.argv[2])
 
-MAX_DISTANCE = distance.euclidean([0, 0], [1, 1])
-
 data = np.random.rand(n, 2)
 centers = data[:k]
 
@@ -20,22 +18,18 @@ stop = False
 clusters = None
 while not stop:
     stop = True
-    clusters = [[[], []] for x in range(k)]
+    clusters = [[[], []] for _ in range(k)]
 
+    # divide into clusters
     for vector in data:
-        cluster_index = 0
-        class_distance = MAX_DISTANCE
-        for i in range(k):
-            current_distance = distance.euclidean(vector, centers[i])
-            if class_distance > current_distance:
-                cluster_index = i
-                class_distance = current_distance
-        clusters[cluster_index][0].append(vector[0])
-        clusters[cluster_index][1].append(vector[1])
+        cluster = clusters[np.argmin([distance.euclidean(vector, centers[i]) for i in range(k)])]
+        cluster[0].append(vector[0])
+        cluster[1].append(vector[1])
 
+    # calc centers
     for i in range(k):
         next_center = np.average(clusters[i], axis=1)
-        if any(centers[i] != next_center):
+        if not np.array_equal(centers[i], next_center):
             centers[i] = next_center
             stop = False
 
@@ -43,3 +37,4 @@ for cluster in clusters:
     plt.scatter(cluster[0], cluster[1])
 plt.scatter([vector[0] for vector in centers], [vector[1] for vector in centers], color='black')
 plt.show()
+
