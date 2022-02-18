47505056d9ab3aab99b580a331d4a3e638beffaf
Киселёв Андрей <vkusnaya.travka@gmail.com>
2022-02-19 01:13:56 +0300

    code refactoring

--- Potential classifier/main.py
+++ Potential classifier/main.py
@@ -11,59 +11,65 @@ for i in range(n):
     data[i][0] = float(input('x1: '))
     data[i][1] = float(input('x2: '))
 
+kx = np.zeros(4) # K0(x) = 0
+iteration = 0
+successful_sequence = 0
+
 def apply_vec(func_vec, vec):
     func_vec[1] *= vec[0]
     func_vec[2] *= vec[1]
     func_vec[3] *= vec[0] * vec[1]
+    return func_vec
 
-def calc_with_vec(func_vec, vec):
+def calc_vec(func_vec, vec):
     return func_vec[0] + func_vec[1] * vec[0] + func_vec[2] * vec[1] + func_vec[3] * vec[0] * vec[1]
 
-# K0(x) = 0
-kx = np.zeros(4)
+while (successful_sequence < n) and (iteration < 1000):
+    i = iteration % n
+    is_first_class = i < first_class_n
 
-p = 1
-stop = False
-while not stop:
-    stop = True
-    for i in range(n - 1):
-        kxx = np.array([1, 4, 4, 16])  #K(x, x) = 1 + 4x1x1i + 4x2x2i + 16x1x1ix2x2i
+    is_positive = calc_vec(kx, data[i]) > 0
 
-        if p != 0:
-            apply_vec(kxx, data[i])
-            kx += p * kxx
+    if is_positive != is_first_class:
+        successful_sequence = 0
 
-        next_vec_class = 0 if i + 1 < first_class_n else 1
-        is_positive = calc_with_vec(kx, data[i + 1]) > 0
-        if is_positive == (next_vec_class == 0):
-            p = 0
-        else:
-            stop = False
-            if next_vec_class == 0:
-                p = 1
-            else:
-                p = -1
+        kxx = np.array([1, 4, 4, 16]) # K(x, x) = 1 + 4x1x1i + 4x2x2i + 16x1x1ix2x2i
+        p = (1 if is_first_class else -1)
+        kx += p *  apply_vec(kxx, data[i])
 
+    else:
+        successful_sequence += 1
+
+    iteration += 1
+
+print(f'{kx[0]} {kx[1]:+}*x1 {kx[2]:+}*x2 {kx[3]:+}*x1*x2')
+
+#start of plotting
 
 border = 4
 random_data = np.random.rand(500, 2) * border * 2 - border
 ax = plt.gca()
 ax.set_xlim([-border, +border])
 ax.set_ylim([-border, +border])
+plt.xlabel('first class - green, second class - orange')
 for i in range(random_data.shape[0]):
-    if calc_with_vec(kx, random_data[i]) > 0:
+    if calc_vec(kx, random_data[i]) > 0:
         plt.scatter(random_data[i][0], random_data[i][1], color="green")
     else:
         plt.scatter(random_data[i][0], random_data[i][1], color="orange")
 
-try:
-    x = np.arange(-border, -kx[2] / kx[3] - 0.01, 0.01)
+if kx[3] != 0:
+    x = np.arange(-border, -kx[2] / kx[3] - 0.0001, 0.01)
     y = (-kx[1] * x - kx[0]) / (kx[3] * x + kx[2])
     plt.plot(x, y, color='red')
 
-    x = np.arange(-kx[2] / kx[3] + 0.01, border, 0.01)
+    x = np.arange(-kx[2] / kx[3] + 0.0001, border, 0.01)
     y = (-kx[1] * x - kx[0]) / (kx[3] * x + kx[2])
     plt.plot(x, y, color='red')
 
-finally:
-    plt.show()
+else:
+    x = np.arange(-border, border, 0.01)
+    y = (-kx[1] * x - kx[0]) / kx[2]
+    plt.plot(x, y, color='red')
+
+plt.show()
